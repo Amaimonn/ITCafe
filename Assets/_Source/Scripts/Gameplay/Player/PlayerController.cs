ce2f0d58d06357be1e1d8d3d9d33939b74f681f6
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2025-09-15 00:02:35 +0300

    PlayerController RotateToLook

--- Assets/_Source/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_Source/Scripts/Gameplay/Player/PlayerController.cs
@@ -4,16 +4,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField, Min(0f)] private float _moveSpeed = 95f;
-    [SerializeField, Min(0f)] private float _rotationSpeed = 10f;
-    [SerializeField] private Transform _verticalRotationTransform;
     [SerializeField] private Camera _camera;
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private InputActionReference _moveAction;
     [SerializeField] private InputActionReference _rotationAction;
 
     private Vector2 _moveInput;
-    // private Vector2 _rotationInput;
-    // private float _pitch;
 
     private void Start()
     {
@@ -25,12 +21,12 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         _moveInput = _moveAction.action.ReadValue<Vector2>();
-        // _rotationInput = _rotationAction.action.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
         Move(_moveInput * Time.fixedDeltaTime);
+        RotateToLook();
         
         _rigidbody.angularVelocity = Vector3.zero;
     }
@@ -50,39 +46,11 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// Поворот по горизонтали и вертикали
     /// </summary>
-    // private void RotateToTarget()
-    // {
-    //     // Поворачиваемся в направлении камеры
-    //     var cameraProjection = Vector3.ProjectOnPlane(_camera.transform.forward, Vector3.up).normalized;
-    //     var cameraRotation = Quaternion.LookRotation(cameraProjection);
-    //     _rigidbody.MoveRotation(cameraRotation);
-    // }
-
-    private void RotateVertical(Vector2 rotationInput)
+    private void RotateToLook()
     {
-        _verticalRotationTransform.Rotate(Vector3.right, -rotationInput.y * _rotationSpeed);
-
-        // _pitch -= rotationInput.y * _rotationSpeed;
-        // _pitch = Mathf.Clamp(_pitch, -80, 80);
-        // Ограничиваем вращение по вертикали
-        // _verticalRotationTransform.localEulerAngles = new Vector3(_pitch, _verticalRotationTransform.localEulerAngles.y, 0);
-
-        // Получаем нормализованный угол (-180 до 180)
-        var currentAngleX = NormalizeAngle(_verticalRotationTransform.localEulerAngles.x);
-
-        // Ограничиваем вращение по вертикали
-        if (currentAngleX > 80f)
-            _verticalRotationTransform.localEulerAngles = new Vector3(80f, 0, 0);
-        else if (currentAngleX < -80f)
-            _verticalRotationTransform.localEulerAngles = new Vector3(-80f, 0, 0);
+        // Поворачиваемся в направлении камеры
+        var cameraProjection = Vector3.ProjectOnPlane(_camera.transform.forward, Vector3.up).normalized;
+        var cameraRotation = Quaternion.LookRotation(cameraProjection);
+        _rigidbody.MoveRotation(cameraRotation);
     }
-
-    private float NormalizeAngle(float angle)
-    {
-        angle %= 360f;
-        if (angle > 180f)
-            angle -= 360f;
-        return angle;
-    }
-
 }
