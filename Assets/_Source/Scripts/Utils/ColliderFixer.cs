9f194769a88bfcc87c07912bef36278c20177b15
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2025-09-15 18:36:37 +0300

    Collider fix

---
+++ Assets/_Source/Scripts/Utils/ColliderFixer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ColliderFixer : MonoBehaviour
+{
+    private void Start()
+    {
+        var boxColliders = GetComponentsInChildren<BoxCollider>();
+        foreach (var boxCollider in boxColliders)
+        {
+            var fixCenter = boxCollider.center;
+            if (fixCenter.x < 0)
+                fixCenter.x *= -1;
+            if (fixCenter.y < 0)
+                fixCenter.y *= -1;
+            if (fixCenter.z < 0)
+                fixCenter.z *= -1;
+
+            boxCollider.center = fixCenter;
+
+            var fixSize = boxCollider.size;
+            if (fixSize.x < 0)
+                fixSize.x *= -1;
+            if (fixSize.y < 0)
+                fixSize.y *= -1;
+            if (fixSize.z < 0)
+                fixSize.z *= -1;
+
+            boxCollider.size = fixSize;
+        }
+    }
+}
