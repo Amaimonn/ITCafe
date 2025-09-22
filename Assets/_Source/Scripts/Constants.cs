426223c6a642a7774e7b539df907674d111137f2
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2025-09-22 21:49:24 +0300

    Constants file. Single Outline style parameters for all scene objects. Plant collider improvement

---
+++ Assets/_Source/Scripts/Constants.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace ITCafe
+{
+    public static class Constants
+    {
+        #region Outline config
+
+        public const float OUTLINE_WIDTH = 7f;
+        public static Color OUTLINE_COLOR = new Color(245, 255, 30);
+
+        #endregion
+    }
+}
