319893a54b1f318c40f4ac522952d668bd4fb18b
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2025-10-04 18:06:44 +0300

    Kitchen leveling. Items container logic. ItemPicker fsm

---
+++ Assets/_Source/Scripts/Gameplay/Environment/Items/ItemStub.cs
@@ -0,0 +1,42 @@
+using ITCafe.Player;
+using UnityEngine;
+
+namespace ITCafe.Environment
+{
+    public class ItemStub : IItem
+    {
+        public static ItemStub Default = new ItemStub();
+        public Transform transform { get; }
+        public Vector3 CenterOffset { get; }
+        
+        public void Focus()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UnFocus()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CanInteract(PlayerContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Interact(PlayerContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Drop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetPhysicsEnabled(bool isEnabled)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
