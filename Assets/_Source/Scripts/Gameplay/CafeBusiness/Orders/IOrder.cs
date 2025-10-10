f3427f99091542545ea90249394c06b524617bc7
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2025-10-11 00:43:47 +0300

    notes for the future

--- Assets/_Source/Scripts/Gameplay/CafeBusiness/Orders/IOrder.cs
+++ Assets/_Source/Scripts/Gameplay/CafeBusiness/Orders/IOrder.cs
@@ -6,11 +6,11 @@ namespace ITCafe.CafeBusiness
     public interface IOrder
     {
         public bool IsCompleted { get; }
-        public IEnumerable<int> OrderHashes { get; }
+        public IEnumerable<int> OrderHashes { get; } // Remove It
 
         public bool IsCorresponds(int hash)
         {
-            return OrderHashes.Contains(hash);
+            return OrderHashes.Contains(hash); // Make abstract
         }
 
         public bool TryHandOver(int hash);
