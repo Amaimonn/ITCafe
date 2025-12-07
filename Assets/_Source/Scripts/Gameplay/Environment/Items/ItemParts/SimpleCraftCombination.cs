3cd3ac43475c3646eb006cf529bf15b0812001c8
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2025-12-07 14:13:20 +0300

    Craft Icons map. Common (Simple) Craft combination realiaztion (+ prefab). HotDog added

--- Assets/_Source/Scripts/Gameplay/Environment/Items/ItemParts/SimpleCraftCombination.cs
+++ Assets/_Source/Scripts/Gameplay/Environment/Items/ItemParts/SimpleCraftCombination.cs
@@ -1,6 +1,8 @@
-using System.Linq;
 using DevKit.Utils;
 using ITCafe.Data.Items;
+using ITCafe.Gameplay.Data;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace ITCafe.Environment
 {
@@ -12,12 +14,54 @@ namespace ITCafe.Environment
     {
         public override ItemTag Tag => ItemTag.SimpleCombination;
 
+        [SerializeField] private Transform _uiHolder;
+        [SerializeField] private UIDocument _uiDocument;
+        [SerializeField] private AllCraftIconsSO _craftIconsSO;
+
+        private VisualElement _iconsContainer;
+        private Transform _lookAtTarget;
+
         protected override void OnInit()
         {
-#if UNITY_EDITOR
-            var amount = _partsAmountMap.Sum(kp => kp.Value);
-            FLogger.Log<SimpleCraftCombination>(amount.ToString());
-#endif
+            _lookAtTarget = Camera.main.transform;
+
+            var root = _uiDocument.rootVisualElement;
+            _iconsContainer = root.Q<VisualElement>("ImagesContainer");
+            _iconsContainer.Clear();
+            
+            var iconsMap = _craftIconsSO.CraftIconsMap;
+            foreach (var (partTag, amount) in _partsAmountMap)
+            {
+                if (iconsMap.TryGetValue(partTag, out var icon))
+                {
+                    AddIcons(icon, amount);
+                }
+                else
+                {
+                    FLogger.LogWarning($"No craft icon found for {partTag}");
+                    AddIcons(null, amount);
+                }
+            }
         }
+
+        private void AddIcons(Sprite icon, int amount)
+        {
+            for (var i = 0; i < amount; i++)
+            {
+                var iconElement = new VisualElement();
+                if (icon != null)
+                    iconElement.style.backgroundImage = new StyleBackground(icon);
+
+                iconElement.AddToClassList("order-cloud__item-image");
+                _iconsContainer.Add(iconElement);
+            }
+        }
+
+#region MonoBehaviour
+        private void Update()
+        {
+            _uiHolder.transform.LookAt(_lookAtTarget.position, _lookAtTarget.up);
+        }
+#endregion
     }
 }
