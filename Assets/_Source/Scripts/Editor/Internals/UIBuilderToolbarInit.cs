779c562b7748cb7c25feda625f4c89bcf02b588e
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2026-01-08 13:28:30 +0300

    UI Builder toolbar uss menu modification

---
+++ Assets/_Source/Scripts/Editor/Internals/UIBuilderToolbarInit.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace ITCafe.Editor.Internals
+{
+    internal class UIBuilderToolbarInit
+    {
+        [InitializeOnLoadMethod]
+        private static void ModifyAddUssPath()
+        {
+            UIBuilderExtension.ModifyAddUssPath($"{Application.dataPath}/_Source/UIToolkit");
+        }
+        
+        [InitializeOnLoadMethod]
+        private static void ModifyCreateNewUssPath()
+        {
+            UIBuilderExtension.ModifyCreateUssPath($"{Application.dataPath}/_Source/UIToolkit");
+        }
+    }
+}
