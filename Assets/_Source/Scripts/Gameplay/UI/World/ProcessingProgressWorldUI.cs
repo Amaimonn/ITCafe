5b11e585f9da4ac92e7e82ff65df0562f195fe03
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2026-03-01 13:36:48 +0300

    ProcessingProgressWorldUI: look rotation removed

--- Assets/_Source/Scripts/Gameplay/UI/World/ProcessingProgressWorldUI.cs
+++ Assets/_Source/Scripts/Gameplay/UI/World/ProcessingProgressWorldUI.cs
@@ -6,29 +6,19 @@ namespace ITCafe.Gameplay.UI.World
     public class ProcessingProgressWorldUI : MonoBehaviour
     {
         [SerializeField] private UIDocument _worldDocument;
-        [SerializeField] private Transform _uiHolder;
         [SerializeField] private string _progressBarName = "ProgressBar";
 
-        private Transform _lookAtTarget;
         private VisualElement _root;
         private VisualElement _progressBar;
 
         private void Awake()
         {
-            _lookAtTarget = Camera.main.transform;
             _root = _worldDocument.rootVisualElement;
             _progressBar = _root.Q<VisualElement>(name: _progressBarName);
             
             SetProgressUI(0);
             Hide();
         }
-
-#region MonoBehaviour
-        private void Update()
-        {
-            _uiHolder.rotation = Quaternion.LookRotation(_uiHolder.position - _lookAtTarget.position);
-        }
-#endregion
 
         public void SetProgress(float remainingTimeNormalized)
         {
