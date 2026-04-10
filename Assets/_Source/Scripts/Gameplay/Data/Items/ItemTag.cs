212a81580d03a0df7f9dd4fce51c1d85a729a13b
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2026-04-10 21:20:57 +0300

    bacon, bacon_kebab added

--- Assets/_Source/Scripts/Gameplay/Data/Items/ItemTag.cs
+++ Assets/_Source/Scripts/Gameplay/Data/Items/ItemTag.cs
@@ -3,8 +3,8 @@ namespace ITCafe.Data.Items
     public enum ItemTag
     {
         Undefined = 0,
-        Muffin = 1,
-        Croissant = 2,
+        // Muffin = 1,
+        // Croissant = 2,
         Onigiri = 3,
         Fries = 4,
         ChocolateDonut = 5,
@@ -19,18 +19,19 @@ namespace ITCafe.Data.Items
         Egg = 15,
         Tomato = 16,
         Dough = 17,
-        SoupBase = 18,
+        // SoupBase = 18,
         Noodles = 19,
-        Meatloaf = 20,
+        // Meatloaf = 20,
         BasePizzaRaw = 21,
-        HuntersPizzaRaw = 22,
+        // HuntersPizzaRaw = 22,
         RamenRaw = 23,
+        Bacon = 24,
         Cookie = 30,
 
         // Combinations
         SimpleCombination = 5000,
         BurgerCombination = 5001,
-        HotDogCombination = 5002,
+        // HotDogCombination = 5002,
 
         // Menu Items
         HotDog = 10_000,
@@ -39,15 +40,16 @@ namespace ITCafe.Data.Items
         Sushi = 10_003,
         Ramen = 10_004,
         MargheritaPizza = 10_005,
-        HuntersPizza = 10_006,
-        Salad = 10_007,
+        // HuntersPizza = 10_006,
+        // Salad = 10_007,
         TomatoSoup = 10_008,
         Coffee = 10_009,
+        BaconKebab = 10_010,
 
         // Special
         Tray = 15_000,
-        Plate = 15_001,
-        SushiTable = 15_002,
+        // Plate = 15_001,
+        // SushiTable = 15_002,
         Bowl = 15_003,
         PlasticCup = 15_004,
         
@@ -59,7 +61,8 @@ namespace ITCafe.Data.Items
         LettuceSliced = 20_004,
         TomatoSliced = 20_005,
         SalmonSliced = 20_006,
-        RiceBoiled = 20_007,
-        MeatloafSliced = 20_008
+        // RiceBoiled = 20_007,
+        // MeatloafSliced = 20_008,
+        BaconFried = 20_009,
     }
 }
